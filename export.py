15d1ec5afac4f02c1121961f43fee4c29eb99dbd
Kiichigo <106779934+Kiichigo-cc@users.noreply.github.com>
2023-02-27 21:36:03 -0800

    update

--- export.py
+++ export.py
@@ -1,22 +1,38 @@
-import sys
-import json
-
-search_input = str(sys.argv[1])
-database = str(sys.argv[2])
-
-f = open(database)
-data = json.load(f)
-results = []
+f = open("input.txt", "r")
+inputs = f.readlines()
 
 f.close()
 
-for i in data:
-    if search_input.lower() in i['albumName'].lower():
-        results.append(i['albumName'])
+f = open("output.txt", "w")
+f.write("======================\n\n")
+f.write("       " + inputs[0] + "\n")
+inputs.pop(0)
 
-f2 = open('output.txt', 'r+')
+questions_list = []
+question_count = 0
+for i in inputs:
+    if 'Q.' in i:
+        question_count += 1
+        questions_list.append(i)
+        inputs.remove(i)
 
-for i in results:
-    f2.write(i + '\n')
+print(question_count)
+print(inputs)
 
-f2.close
+sublists = []
+temp = []
+for i in inputs:
+    if i[0] == 'A':
+        if temp:
+            sublists.append(temp)
+        temp = []
+    temp.append(i)
+sublists.append(temp)
+
+print(sublists)
+
+for i in range(question_count):
+    f.write("====* Question " + str(i + 1) + " *====\n")
+    f.write(questions_list[i])
+    for j in sublists[i]:
+        f.write(j)
